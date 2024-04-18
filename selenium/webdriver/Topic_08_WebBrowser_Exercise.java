433bcc8dfd07d85e4e463025e1544b6c16cb1c5e
Thuy Vy Nguyen <thuyvy.nguyen1112@gmail.com>
2024-04-18 18:49:51 +0700

    Modified Topic_02_Selenium_Locator

--- selenium/webdriver/Topic_08_WebBrowser_Exercise.java
+++ selenium/webdriver/Topic_08_WebBrowser_Exercise.java
@@ -17,7 +17,7 @@ public class Topic_08_WebBrowser_Exercise {
     }
 
     @Test
-    public void TC_01_URL(){
+    public void TC_01_Verify_URL(){
         driver.get("http://live.techpanda.org/");
 
         driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
@@ -30,7 +30,7 @@ public class Topic_08_WebBrowser_Exercise {
     }
 
     @Test
-    public void TC_02_Title(){
+    public void TC_02_Verify_Title(){
         driver.get("http://live.techpanda.org/");
 
         driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
@@ -40,6 +40,38 @@ public class Topic_08_WebBrowser_Exercise {
         driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
 
         Assert.assertEquals(driver.getTitle(),"Create New Customer Account");
+    }
+
+    @Test
+    public void TC_03_Navigate_Function(){
+        driver.get("http://live.techpanda.org/");
+
+        driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+
+        driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/create/");
+
+        driver.navigate().back();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/login/");
+
+        driver.navigate().forward();
+
+        Assert.assertEquals(driver.getTitle(),"Create New Customer Account");
+    }
+
+    @Test
+    public void TC_04_Get_Page_Source_Code(){
+        driver.get("http://live.techpanda.org/");
+
+        driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+
+        Assert.assertTrue(driver.getPageSource().contains("Login or Create an Account"));
+
+        driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+
+        Assert.assertTrue(driver.getPageSource().contains("Create an Account"));
     }
 
 
