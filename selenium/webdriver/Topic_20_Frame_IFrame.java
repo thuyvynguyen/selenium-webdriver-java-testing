2678990e75debdcee900d8ddadec327211134ede
Thuy Vy Nguyen <thuyvy.nguyen1112@gmail.com>
2024-06-10 22:23:08 +0700

    Modified an Added

---
+++ selenium/webdriver/Topic_20_Frame_IFrame.java
@@ -0,0 +1,43 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_20_Frame_IFrame {
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser(){
+        driver = new FirefoxDriver();
+        driver.get("https://demo.nopcommerce.com/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_IFrame_FormSite() throws InterruptedException {
+        //HTML Web A
+        driver.get("https://www.formsite.com/templates/education/campus-safety-survey/");
+
+        driver.findElement(By.cssSelector("img[alt='Campus Safety Survey']")).click();
+        Thread.sleep(4000);
+
+        //Select on HTML web B
+        new Select(driver.findElement(By.cssSelector("select#RESULT_RadioButton-2"))).selectByVisibleText("Freshman");
+        Thread.sleep(3000);
+    }
+    @Test
+    public void TC_02(){
+
+    }
+
+    @AfterClass
+    public void cleanBrowser() {driver.quit();}
+}
